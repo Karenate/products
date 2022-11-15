69a14345192b8c58f0068ccb9ed19b1afd2a217c
Karen Wu <oneaboro@gmail.com>
2022-11-15 00:32:02 -0600

    add products.py

---
+++ products.py
@@ -0,0 +1,24 @@
+# While Loop通常適合: 執行未知次數的情況下使用
+
+products = []
+while True:
+	name = input('請輸入商品名稱： ')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格:　')
+
+    # 方法1
+   	# p = []
+	# p.append(name)
+    # p.append(price)
+    # products.append(p)
+     
+    # 方法2
+    # p = [name, price]
+    # products.append(p)
+
+    # 方法三
+    # products.append(p)
+	products.append([name,price])
+	
+print(products)
