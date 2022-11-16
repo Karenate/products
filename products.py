95e1493ae8f9c28a06567d7db357278470dca2b5
Karen Wu <oneaboro@gmail.com>
2022-11-16 17:37:21 -0600

    write a file

--- products.py
+++ products.py
@@ -25,3 +25,11 @@ print(products)
 
 for p in products:
 	print(p[0], '的價格是', p[1])
+
+# 寫入檔案
+# +法 字串合併、\n 換行
+# 檔案格式為csv檔
+
+with open('products.csv', 'w') as f:
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
